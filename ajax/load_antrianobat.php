2ea3ae2556f30e70e29d778199ee5da50edde5e6
AntriQue <pratamahm@gmail.com>
2023-06-22 19:19:12 +0700

    update antrian obat

--- ajax/load_antrianobat.php
+++ ajax/load_antrianobat.php
@@ -1,4 +1,8 @@
 <?php 
+require __DIR__."/../vendor/autoload.php";
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__."/../");
+$dotenv->load();
+
     $koneksi = mysqli_connect($_SERVER['DB_HOST'], $_SERVER['DB_USER'], $_SERVER['DB_PASSWORD'], $_SERVER['DB_NAME']);
     $query_tampil = mysqli_query($koneksi, "SELECT * FROM tbl_daftarpasien where tgl_daftar = CURDATE() AND status_masuk='rawat' ORDER BY no_antrian DESC")  ;
     $rows = array();
